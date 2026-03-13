ad142c8a72d13323338fc0ec17481a38dd6c9f05
Charoo3 <charoonegi31@gmail.com>
2026-03-13 10:24:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function validateForm()
+{
+
+var name=document.getElementById("name").value;
+var email=document.getElementById("email").value;
+
+if(name=="" || email=="")
+{
+alert("Please fill all fields");
+return false;
+}
+
+alert("Form Submitted Successfully");
+
+return true;
+
+}
